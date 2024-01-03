b441d49e26210cd8795c54772a3edee1ae2e4a0f
cuddlyclara <138817611+cuddlyclara@users.noreply.github.com>
2024-01-03 20:55:12 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        if(Clipboard.ContainsText())
+        {
+            Clipboard.SetText(Clipboard.GetText()); // refactor data
+        }
+    }
+}
